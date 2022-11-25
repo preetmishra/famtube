c642095ee4fe6c9f4227e29c8058b1426781c40d
Preet Mishra <preet@navigus.in>
2022-11-25 19:36:24 +0530

    Explicitly send pagination payload in response for /videos API.

--- api/services/videos.services.js
+++ api/services/videos.services.js
@@ -1,5 +1,9 @@
 const Video = require("../models/video");
 
+const countDocuments = async ({ filter = {} }) => {
+  return await Video.find(filter).count();
+};
+
 const findAll = async ({ filter = {}, pageNumber = 0, pageLimit = 5 }) => {
   return await Video.find(filter)
     .sort({ publishedAt: -1 })
@@ -8,7 +12,14 @@ const findAll = async ({ filter = {}, pageNumber = 0, pageLimit = 5 }) => {
 };
 
 const fetchVideos = async ({ pageNumber, pageLimit }) => {
-  return await findAll({ pageNumber, pageLimit });
+  return {
+    data: await findAll({ pageNumber, pageLimit }),
+    pagination: {
+      total: await countDocuments({}),
+      currentPageNumber: pageNumber,
+      pageLimit,
+    },
+  };
 };
 
 module.exports = {
